8c74ba87868b4a4cb67778a9c9a5210e41548444
oluzingh <adeyanjuoluzingh@gmail.com>
2022-03-10 10:10:25 -0800

    ‘06’

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
- *
- * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
+ * main - print the sizeof the various type
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-char c;
-int i;
-long li;
-long long lli;
-float f;
-printf("Size of a char:%ld byte(s)\n", sizeof(c));
-printf("Size of an int:%ld byte(s)\n", sizeof(i));
-printf("Size of a long int:%ld byte(s)\n", sizeof(li));			
-printf("Size of a long long int:%ld byte(s)\n", sizeof(lli));
-printf("Size of a float:%ld byte(s)\n", sizeof(f));
+printf("Size of a char: %d byte(s)\n", sizeof(c));
+printf("Size of an int: %d byte(s)\n", sizeof(i));
+printf("Size of a long int: %d byte(s)\n", sizeof(li));			
+printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+printf("Size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 }
