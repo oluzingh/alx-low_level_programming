9b870a04fb40d8b406f06fa6bec3ae419f6d1ad1
oluzingh <adeyanjuoluzingh@gmail.com>
2022-03-10 09:07:44 -0800

    ‘04’

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <studio.h>
-/* This code outputs text */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts"\"Programming is like building a multilingual puzzle";
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
