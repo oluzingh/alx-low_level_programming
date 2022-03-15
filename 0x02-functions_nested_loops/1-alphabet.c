e07269092f74d48e5be890872269993e724ab21b
oluzingh <adeyanjuoluzingh@gmail.com>
2022-03-15 07:59:48 -0700

    ‘001’

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
-#include "oluzingh"
+#include "main.h"
 /**
- * main - Entry Point 
+ * main - print_alphabet - prints the alphabet in lowercase, followed by a new line.
  *
  * Return: 0
  */
 void print_alphabet(void)
 {
-	char c;
-	for(c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-_putchar('\n');
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	
+	_putchar('\n');
 }
