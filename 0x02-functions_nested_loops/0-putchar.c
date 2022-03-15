0a45e896e240bd9e655b8f7138b5f4ca0859f0d3
oluzingh <adeyanjuoluzingh@gmail.com>
2022-03-15 04:52:40 -0700

    ‘00’

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
-#include "oluzingh.h"
+#include "holberton.h"
 
 /**
  * main - function to print
  *
  * Return: 0
  */
-int main(void)
+	int main(void)
 {
-	char oluzingh[8] = {'o', 'l', 'u', 'z', 'i', 'n', 'g', 'h'};
+	char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
 	unsigned int c;
 
-	for (c = 0; c < sizeof(oluzingh); c++)
+	for (c = 0; c < sizeof(holberton); c++)
 {
-	_putchar(oluzingh[c]);
+	_putchar(holberton[c]);
 }
 	_putchar('\n');
 	return (0);
