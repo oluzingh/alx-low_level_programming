f23034128288899a93ec5f1405a83eae20eea7bd
oluzingh <adeyanjuoluzingh@gmail.com>
2022-03-16 01:21:15 -0700

    ‘001’

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
- * main - print_alphabet - prints the alphabet in lowercase, followed by a new line.
- *
- * Return: 0
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
