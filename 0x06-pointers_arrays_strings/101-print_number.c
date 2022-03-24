39dd1f8478b30aa68dd170d619009adc16b19fe9
oluzingh <adeyanjuoluzingh@gmail.com>
2022-03-24 16:30:35 -0700

    ‘09’

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,29 +6,18 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
-
+	unsigned int num;
+	/*check if numbrt is negative*/
+	num = n;
 	if (n < 0)
 	{
 		_putchar(45);
-		m = n * -1;
+		num = -n;
 	}
-	else
+	/* print number by recursion*/
+	if (num / 10)
 	{
-		m = n;
+		print_number(number / 10);
 	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	_putchar((num % 10) + '0');
 }
