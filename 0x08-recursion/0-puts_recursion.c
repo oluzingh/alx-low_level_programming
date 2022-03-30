1bcc14630072b16643671062d833cbb1a18e36c2
oluzingh <adeyanjuoluzingh@gmail.com>
2022-03-30 06:31:58 -0700

    01

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,10 @@
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
-
 	{
-		putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar(*s);
+		return;
 	}
-	else
-		_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
